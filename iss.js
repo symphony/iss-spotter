c087b1fba4848c811b37e1024b98984683cc8cbe
symphony <jciurc@gmail.com>
2022-04-14 22:45:43 -0600

    finished callback version

--- iss.js
+++ iss.js
@@ -2,30 +2,30 @@ const request = require('./node_modules/request');
 
 const checkErrors = (error, status, data, callback) =>  {
   if (error) throw callback(error, status);
-  if (status?.statusCode !== 200) throw callback('Bad request', status);
-  if (!data.includes("}")) throw callback("Return value not JSON", status);
+  if (status?.statusCode !== 200) throw callback("Bad request", status, data);
+  if (!data.includes("}")) throw callback("Return value not JSON", status, data);
 };
 
 const fetchMyIP = (callback) => {
   request('https://api.IPify.org?format=json', (error, status, data) => {
     checkErrors(error, status, data, callback);
-    callback(error, JSON.parse(data).ip);
+    callback(error, status, JSON.parse(data).ip);
   });
 };
 
 const fetchCoordsByIP = (IP, callback) => {
   request(`https://freegeoip.app/json/${IP}`, (error, status, data) => {
-    checkErrors(error, status, data, callback)
+    checkErrors(error, status, data, callback);
     const {latitude, longitude} = JSON.parse(data);
-    callback(error, {latitude, longitude});
+    callback(error, status, {latitude, longitude});
   });
 };
 
 const fetchIssFlyoverTimes = (geo, callback) => {
   request(`https://iss-pass.herokuapp.com/json/?lat=${geo.latitude}&lon=${geo.longitude}`, (error, status, data) => {
-    checkErrors(error, status, data, callback)
+    checkErrors(error, status, data, callback);
     const {response} = JSON.parse(data);
-    callback(error, response);
+    callback(error, status, response);
   });
 };
 
--- test/issTest.js
+++ test/issTest.js
@@ -3,7 +3,7 @@ const { assert } = require('chai');
 
 describe('fetchMyIP()', () => {
   it('returns your IP as a string', (done) => {
-    fetchMyIP((err, data) => {
+    fetchMyIP((err, status, data) => {
       const dataType = "string";
       assert.equal(err, null);
       assert.equal(dataType, typeof(data));
@@ -14,7 +14,7 @@ describe('fetchMyIP()', () => {
 
 describe('fetchCoordsByIP()', () => {
   it('returns your coordinates in an object', (done) => {
-    fetchCoordsByIP("192.0.0.1", (err, data) => {
+    fetchCoordsByIP("192.0.0.1", (err, status, data) => {
       assert.equal(err, null);
       assert.notDeepEqual(data.latitude, undefined);
       assert.notDeepEqual(data.longitude, undefined);
@@ -25,19 +25,20 @@ describe('fetchCoordsByIP()', () => {
 
 describe('fetchIssFlyoverTimes()', () => {
   it('returns flyover times as an array', (done) => {
-    fetchIssFlyoverTimes({ latitude: 50, longitude: -100 }, (err, data) => {
+    fetchIssFlyoverTimes({ latitude: 50, longitude: -100 }, (err, status, data) => {
       assert.equal(err, null);
       assert.notDeepEqual(data.length === 0);
       assert.notDeepEqual(data[0].risetime, undefined);
       assert.notDeepEqual(data[0].duration, undefined);
       done();
-    }),
-    it('returns an error when bad request', (done) => {
-      fetchIssFlyoverTimes({}, (err, data) => {
-        // assert.equal(err, null);
-        assert.notDeepEqual(data.statusCode, 200);
-        done();
-      });
+    });
+  }),
+
+  it('throws an error when bad request', (done) => {
+    fetchIssFlyoverTimes({}, (err, status, data) => {
+      assert.deepEqual("invalid coordinates", data);
+      assert.notDeepEqual(200, status.statusCode);
+      done();
     });
   });
 });
